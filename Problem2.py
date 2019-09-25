7f0b773658d4aa966948fce47c384cac1f23f32b
stephanieboroshok <sb8485a@american.edu>
2019-09-25 10:32:49 -0400

    initial attempt

--- Problem2.py
+++ Problem2.py
@@ -1,1 +1,20 @@
 # turtle
+# import turtle
+# create turtle
+# draw things
+#create a fucntion that takes in parameter length to determine length of square
+
+import turtle
+
+def square (turtle_obj,length):
+    for i in range(4):
+        turtle_obj.fd(length)
+        turtle_obj.rt(90)
+
+length = int(input('Enter the length of the sides of the square:'))
+bob = turtle.Turtle()
+
+square(bob, length)
+
+turtle.done()
+# keeps window open
